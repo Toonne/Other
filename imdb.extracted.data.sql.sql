2245ab608702a945304b61328b61cea5865a7c0a
Toonne <66329917+Toonne@users.noreply.github.com>
2022-04-25 12:54:04 +0200

    Create imdb.extracted.data.sql.sql

---
+++ imdb.extracted.data.sql.sql
@@ -0,0 +1,18 @@
+/*
+
+SQL server script to work with the extracted data from https://github.com/Toonne/Other/blob/main/imdb.extract.data.from.list.js
+
+1. Usage: Replace all ' with ''
+2. Place the content in the variable and run
+
+*/
+
+DECLARE @jsonData NVARCHAR(MAX) = 'Place movies-info.json content here';
+
+SELECT
+	Title = JSON_VALUE([value], '$.title'),
+	ImdbId = JSON_VALUE([value], '$.imdbId'),
+	[Year] = JSON_VALUE([value], '$.year'),
+	Rating = JSON_VALUE([value], '$.rating'),
+	Votes = JSON_VALUE([value], '$.votes')
+FROM OPENJSON(@jsonData, '$') AS MovieList
