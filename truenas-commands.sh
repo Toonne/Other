92230510c8602f727721de718d2e9d728c888c3a
Toonne <66329917+Toonne@users.noreply.github.com>
2023-03-28 14:44:30 +0200

    Create truenas-commands.sh

---
+++ truenas-commands.sh
@@ -0,0 +1,26 @@
+#Temperature summary
+./temperature.sh
+
+#Status and clear any errors on pool
+zpool status -v
+zpool clear Storport
+
+#Smart data status
+smartctl -a /dev/sdb
+smartctl -a /dev/sdc
+smartctl -a /dev/sdd
+smartctl -a /dev/sde
+smartctl -a /dev/sdf
+smartctl -a /dev/sdg
+smartctl -a /dev/sdh
+smartctl -a /dev/sdi
+
+#Format to prepare for use in TrueNAS (also requires a reboot)
+sgdisk -Z /dev/sdj
+sgdisk -Z /dev/sdk
+sgdisk -Z /dev/sdl
+sgdisk -Z /dev/sdm
+sgdisk -Z /dev/sdn
+sgdisk -Z /dev/sdo
+sgdisk -Z /dev/sdp
+sgdisk -Z /dev/sdq
