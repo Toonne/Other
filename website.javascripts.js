3b81c8b849b0a38b9e39223c6dce29348a7d0025
Toonne <66329917+Toonne@users.noreply.github.com>
2023-10-27 14:14:35 +0200

    Update website.javascripts.js

--- website.javascripts.js
+++ website.javascripts.js
@@ -2,3 +2,6 @@
 
 //hide checked cards on Pokellector - https://www.pokellector.com/
 document.querySelectorAll(".card.checked").forEach(e => e.remove());
+
+//remove all texts on top of cards on Pokellector - https://www.pokellector.com/
+document.querySelectorAll(".plaque").forEach(e => e.remove());
