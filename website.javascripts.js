101b16435664f3e46f1c44ee1bce68e3e9efea4e
Toonne <66329917+Toonne@users.noreply.github.com>
2023-11-02 17:34:03 +0100

    Update website.javascripts.js

--- website.javascripts.js
+++ website.javascripts.js
@@ -5,3 +5,6 @@ document.querySelectorAll(".card.checked").forEach(e => e.remove());
 
 //remove all texts on top of cards on Pokellector - https://www.pokellector.com/
 document.querySelectorAll(".plaque").forEach(e => e.remove());
+
+//remove opacity so cards are clearly visible on Pokellector - https://www.pokellector.com/
+document.querySelectorAll(".cardlisting div.card img.card").forEach(e => e.style.opacity = "1");
